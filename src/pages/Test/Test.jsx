d5324af572f1587d7032be37e4f2e0f991921572
G-santanna <gustavo.soliveira@sptech.school>
2024-10-22 21:31:05 -0300

    refactor: style changes na página de cadastro

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ItemList from "../../components/ItemList/ItemList";
+import CardItem from "../../components/CardItem/CardItem";
+import Input from "../../components/Input/Input";
+import Radio from "../../components/Radio/Radio";
+import NavBar from "../../components/NavBar/NavBar";
+import Botao from "../../components/Botao/Botao";
 
 const Test = () => {
     const values = [
@@ -28,8 +33,14 @@ const Test = () => {
 
     return (
         <div>
+            <NavBar />
             <ItemList values={values} />
+            <CardItem title="Escola 01" firstValue="Alunos: 12" secondValue="Pagamentos Restantes: 0" />
+            <Input size={224} type="text" label="Nome" value="" onChange={() => { }} />
+            <Radio size={224} label="Sexo" radioLabels={["Masculino", "Feminino"]} values={["M", "F"]} name="sexo" onChange={() => { }} />
+            <Botao size={150} onClick={() => { }} colorPreset={"white"} hoverPreset={"yellow"}>Minhas Escolas</Botao>
         </div>
+
     );
 };
 
