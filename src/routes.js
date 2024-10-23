1ed0ea8947188138bf573b91ed42d9dd40b368d4
Gustavo Henrique <gustavo329886@gmail.com>
2024-10-22 23:09:33 -0300

    feat: Nova pagina de Escolas

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Test from "./pages/Test/Test";
 import LoginProprietarioServico from "./pages/LoginProprietarioServico/LoginProprietarioServico"
 import CadastroProprietarioServico from "./pages/CadastroProprietarioServico/CadastroProprietarioServico"
+import TelaListagemEscolas from "./pages/TelaListagemEscolas/TelaListagemEscolas";
 
 
 function Rotas() {
@@ -11,6 +12,7 @@ function Rotas() {
           <Routes>
             <Route path="/cadastro" element={<CadastroProprietarioServico />} />
             <Route path="/login" element={<LoginProprietarioServico />} />
+            <Route path="/escolas" element={<TelaListagemEscolas />} />
             <Route path="/test" element={<Test />} />
           </Routes>
         </BrowserRouter>
