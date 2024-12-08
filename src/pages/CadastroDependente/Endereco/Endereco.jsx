8be1c9ba44d9ec8a506eb0fc23628037bcc02adb
Marayah <marayah.pintos@sptech.school>
2024-12-08 20:27:45 -0300

    feat/implementação-viaCEP

--- src/pages/CadastroDependente/Endereco/Endereco.jsx
+++ src/pages/CadastroDependente/Endereco/Endereco.jsx
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Endereco.module.css";
 import Input from "../../../components/Input/Input";
 import InputMask from "react-input-mask";
+import useViaCep from "../../../utils/useViaCep";
+
+
 
 const Endereco = ({responsavelRef}) => {
+
+    const [cep, setCep] = useState("");
+    const { dados, erro, loading, buscarCep } = useViaCep();
+    const [updateKey, setUpdateKey] = useState(0); 
+
+    const handleBuscar = async () => {
+        console.log(cep)
+        if (cep.length === 8 && !isNaN(cep)) {
+            buscarCep(cep)
+            .then((response) => {
+                console.log(response); // Verifique se você está recebendo os dados esperados
+                if (response && !erro) {
+                    responsavelRef.current.endereco.logradouro = response.logradouro || "";
+                    responsavelRef.current.endereco.bairro = response.bairro || "";
+                    responsavelRef.current.endereco.cidade = response.localidade || "";
+                    setUpdateKey((prevKey) => prevKey + 1);
+                } else {
+                   
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar o CEP:", error);
+                alert("Erro ao buscar o CEP. Tente novamente mais tarde.");
+            });
+        } else {
+            
+        }
+    };
+
+    useEffect(()=>{
+        handleBuscar()
+    },[cep])
     return (
-        <div className={styles['container']}>
+        <div className={styles['container']} key={updateKey}>
             <div className={styles['inputs']}>
                 <InputMask
                     mask="99999-999"
                     maskChar={null}
-                    value={responsavelRef.current.endereco.cep}
-                    onChange={(e) => responsavelRef.current.endereco.cep = e.target.value}
-                >
+                    // value={responsavelRef.current.endereco.cep}
+                    // onChange={(e) => responsavelRef.current.endereco.cep = e.target.value}
+                
+                    value={cep}
+                    onChange={(e) => setCep(e.target.value.replace("-", ""))}
+                    >
                     {() =>
                         <Input
                             placeholder="CEP"
